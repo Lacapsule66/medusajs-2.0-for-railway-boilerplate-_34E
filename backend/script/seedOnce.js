b915a6ed716cce6a17bc53767310eacb611ded06
Agence Web La Capsule <132720142+Lacapsule66@users.noreply.github.com>
2024-09-14 00:54:41 +0200

    oik

--- backend/script/seedOnce.js
+++ backend/script/seedOnce.js
@@ -1,11 +1,11 @@
 // /script/seedOnce.js
-require('dotenv').config();
-const axios = require('axios');
-const { exec } = require('child_process');
-const { Client } = require('pg');
+require("dotenv").config();
+const axios = require("axios");
+const { exec } = require("child_process");
+const { Client } = require("pg");
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
 });
 
 const checkIfSeeded = async () => {
@@ -13,11 +13,12 @@ const checkIfSeeded = async () => {
     await client.connect();
     await client.query('SELECT 1 FROM "user" LIMIT 1;');
     return true;
+    const caca = "caca";
   } catch (error) {
     if (error.message.includes('relation "user" does not exist')) {
       return false;
     }
-    console.error('Unexpected error checking if database is seeded:', error);
+    console.error("Unexpected error checking if database is seeded:", error);
     process.exit(1);
   } finally {
     await client.end();
@@ -43,27 +44,28 @@ const runCommand = (command) => {
 
 const seedDatabase = async () => {
   try {
-    console.log('Running migrations...');
-    await runCommand('npx medusa migrations run');
-    
-    console.log('Running link sync...');
-    await runCommand('npx medusa links sync');
-    
-    console.log('Running seed script...');
-    await runCommand('npm run seed');
-    
+    console.log("Running migrations...");
+    await runCommand("npx medusa migrations run");
+
+    console.log("Running link sync...");
+    await runCommand("npx medusa links sync");
+
+    console.log("Running seed script...");
+    await runCommand("npm run seed");
+
     const adminEmail = process.env.MEDUSA_ADMIN_EMAIL;
     const adminPassword = process.env.MEDUSA_ADMIN_PASSWORD;
     if (adminEmail && adminPassword) {
-      console.log('Creating admin user...');
-      await runCommand(`npx medusa user -e "${adminEmail}" -p "${adminPassword}"`);
+      console.log("Creating admin user...");
+      await runCommand(
+        `npx medusa user -e "${adminEmail}" -p "${adminPassword}"`
+      );
     }
-    
-    console.log('Database seeded and admin user created successfully.');
+
+    console.log("Database seeded and admin user created successfully.");
     return;
-    
   } catch (error) {
-    console.error('Failed to seed database or create admin user:', error);
+    console.error("Failed to seed database or create admin user:", error);
     process.exit(1);
   }
 };
@@ -71,10 +73,10 @@ const seedDatabase = async () => {
 const seedOnce = async () => {
   const isSeeded = await checkIfSeeded();
   if (!isSeeded) {
-    console.log('Database is not seeded. Seeding now...');
+    console.log("Database is not seeded. Seeding now...");
     await seedDatabase();
   } else {
-    console.log('Database is already seeded. Skipping seeding.');
+    console.log("Database is already seeded. Skipping seeding.");
   }
 };
 
@@ -84,12 +86,12 @@ const reportDeploy = async () => {
     return;
   }
   const projectId = process.env.RAILWAY_PROJECT_ID;
-  const templateId = 'medusa-2.0';
+  const templateId = "medusa-2.0";
   const payload = { projectId, templateId };
   try {
-      const response = await axios.post(`${url}/api/projectDeployed`, payload);
+    const response = await axios.post(`${url}/api/projectDeployed`, payload);
   } catch (error) {
-      console.error(`An error occurred: ${error.message}`);
+    console.error(`An error occurred: ${error.message}`);
   }
 };
 
